3131915d53c639256930a8c238ddd127237608b0
root <ganireddy651>
2023-05-19 08:15:56 +0000

    Gajdjdj

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -1,3 +1,60 @@
 // Write your code here
+import {Component} from 'react'
+import './index.css'
 
-gshdhd
+class Speedometer extends Component {
+  state = {speed: 0}
+
+  onAccelerate = () => {
+    const {speed} = this.state
+    if (speed < 200) {
+      this.setState(prevState => ({
+        speed: prevState.speed + 10,
+      }))
+    }
+  }
+
+  onBrake = () => {
+    const {speed} = this.state
+    console.log(speed)
+    if (speed > 0) {
+      this.setState(prevState => ({
+        speed: prevState.speed - 10,
+      }))
+    }
+  }
+
+  render() {
+    const {speed} = this.state
+    return (
+      <div className="speed_meter_bg_container">
+        <h1 className="main-heading">Speed Meter</h1>
+        <div className="image-container">
+          <img
+            className="speed-meter-image"
+            src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png "
+            alt="speedometer"
+          />
+          <h1 className="heading">
+            Speed is <span>{speed}</span>mph
+          </h1>
+          <p className="para">Min limit is 0mph, Max limit is 200mph</p>
+          <div className="button-container">
+            <button
+              onClick={this.onAccelerate}
+              className="accelerate-btn"
+              type="button"
+            >
+              Accelerate
+            </button>
+            <button onClick={this.onBrake} className="brake-btn" type="button">
+              Apply Brake
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Speedometer
